c786f88290a4f01bbb231a31077e4312251d6f34
n.andreychenko <natandreychenko@gmail.com>
2019-11-28 19:42:20 +0300

    Fix time string formatting as 'H:mm:ss'

--- tests/index.js
+++ tests/index.js
@@ -104,6 +104,10 @@ describe('formatTime', () => {
     expect(formatTime('1:30', 'H:mm', 'HH:mm:ss')).to.equal('01:30:00');
   });
 
+  it('should return string formatted as \'H:mm:ss\'', () => {
+    expect(formatTime('1:30', 'H:mm', 'H:mm:ss')).to.equal('1:30:00');
+  });
+
   it('should return string formatted as \'mm:ss\'', () => {
     expect(formatTime('11:30', 'mm:ss', 'HH:mm:ss')).to.equal('00:11:30');
   });
