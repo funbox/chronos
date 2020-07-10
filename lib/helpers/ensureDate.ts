4f14385cfbe5ddfda28edd554414dbe8f471c2ee
n.andreychenko <natandreychenko@gmail.com>
2020-07-10 19:03:39 +0300

    Rewrite addDays & ensureDate to TS

--- lib/helpers/ensureDate.ts
+++ lib/helpers/ensureDate.ts
@@ -3,7 +3,7 @@
  * @param {Date|number|string} value
  * @return {Date} Date object result
  */
-export default value => {
+export default (value: Date | number | string) => {
   let dateValue = value;
   // It's possible to pass value as a ISO 8601 or RFC2822 string. Others string formats aren't recommended to pass.
   // Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse
@@ -11,12 +11,12 @@ export default value => {
     dateValue = Date.parse(dateValue);
   }
 
-  const date = new Date((`${dateValue}`).length === 10 ? dateValue * 1e3 : +dateValue);
+  const date = new Date((`${dateValue}`).length === 10 ? +dateValue * 1e3 : +dateValue);
 
   // new Date('something') instanceof Date returns true,
   // so when date === Invalid Date we check that date isn't NaN
   // isNaN isn't secure, but Number.isNaN isn't supported by IE11
-  if (!(date instanceof Date) || isNaN(date)) { // eslint-disable-line no-restricted-globals
+  if (!(date instanceof Date) || isNaN(Number(date))) { // eslint-disable-line no-restricted-globals
     throw new Error(`Invalid date value: ${value}`);
   }
 
