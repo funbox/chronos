8be237a5d0d80b125ed4354eab12f0b01de7709f
n.andreychenko <natandreychenko@gmail.com>
2020-07-13 19:09:34 +0300

    Add ESLint settings for TS linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,31 @@
 module.exports = {
-  extends: '@funboxteam',
+  extends: [
+    '@funboxteam',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: '.',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-useless-constructor': 'error',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/prefer-includes': 'off',
+  },
+  settings: {
+    'import/extensions': ['.ts'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      node: {
+        extensions:['.ts'],
+      }
+    }
+  },
 };
