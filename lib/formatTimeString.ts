0419b507d5d7f1f3ffbd1eb7890e37fed7ab2ed0
igoradamenko <mail@igoradamenko.com>
2020-08-08 00:11:20 +0300

    Fix hour formatting in formatTimeString
    There was an expression:
    
        (time.H ? time.H : `${+time.HH}`) || '0'
    
    that could lead to NaN instead of a correct result
    
    When we have 'H' in the result format, there are three possible ways to solve it:
    
    1. There's 'H' item in the value. In this case it will be returned
    
    2. There's no 'H', but there's 'HH' item in the value. In this case HH will be returned
    
    3. There's neither 'H' nor 'HH' item in the value. In this case the expression above triggers second part of ternary expression and +HH converts to NaN
    
    This commit fixes the issue

--- lib/formatTimeString.ts
+++ lib/formatTimeString.ts
@@ -10,10 +10,12 @@ import parseTime from './helpers/parseTime';
 export default (value: string, valueFormat: string, format: string): string => {
   const time = parseTime(value, valueFormat);
 
+  const hours = time.H || time.HH || 0;
+
   format = format.replace('ss', time.ss || '00');
   format = format.replace('mm', time.mm || '00');
-  format = format.replace('HH', (time.H ? `0${time.H}`.slice(-2) : time.HH) || '00');
-  format = format.replace('H', (time.H ? time.H : `${+time.HH}`) || '0');
+  format = format.replace('HH', `0${hours}`.slice(-2));
+  format = format.replace('H', `${+hours}`);
 
   return format;
 };
