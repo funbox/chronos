23dba804d3f8bcbd93e435b647d026d61571a2c0
igoradamenko <mail@igoradamenko.com>
2023-05-29 14:17:58 +0300

    Fix linting errors; refactor linting settings

--- lib/helpers/ensureDate.ts
+++ lib/helpers/ensureDate.ts
@@ -13,13 +13,13 @@ export const ensureDate = (value: ChronosDate): Date => {
     dateValue = Date.parse(dateValue);
   }
 
-  const date = new Date((`${dateValue}`).length === 10 ? +dateValue * 1e3 : +dateValue);
+  const date = new Date((`${dateValue.toString()}`).length === 10 ? +dateValue * 1e3 : +dateValue);
 
   // new Date('something') instanceof Date returns true,
   // so when date === Invalid Date we check that date isn't NaN
   // isNaN isn't secure, but Number.isNaN isn't supported by IE11
   if (!(date instanceof Date) || isNaN(+date)) { // eslint-disable-line no-restricted-globals
-    throw new Error(`Invalid date value: ${value}`);
+    throw new Error(`Invalid date value: ${value.toString()}`);
   }
 
   return date;
--- tests/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: '../.eslintrc.js',
-  rules: {
-    '@typescript-eslint/no-var-requires': 'off',
-  }
-};
